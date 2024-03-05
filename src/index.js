5fc194c0dbb175afc113d095c27aef28bd6aec67
ShariXan <158981154+ShariXan@users.noreply.github.com>
2024-03-05 16:35:03 +1030

    Default city removed
    Default city removed.
    Search is blank initially

--- src/index.js
+++ src/index.js
@@ -102,5 +102,3 @@ function displayForecast(response) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("");
