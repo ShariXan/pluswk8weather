b3393bdd3baa3250955819a6e91db7cd538febd3
ShariXan <158981154+ShariXan@users.noreply.github.com>
2024-03-05 16:31:48 +1030

    Live api information added to Forecast
    Days of the week added to Forecast, live information returned

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "5fe0ed6aba088342a8t4e2c63oeeb1fb";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -71,7 +78,7 @@ function displayForecast(response) {
         forecastHtml +
         `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <div class="weather-forecast-icon">
          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         </div>
@@ -96,4 +103,4 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("");
